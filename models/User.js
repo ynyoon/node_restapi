1d70f72217b0506d3c23d619b2d45dbf0155fddb
snowynam <snowynam1@gmail.com>
2020-02-15 23:17:01 +0900

    User 모델 생성 및 접속시 유저생성 테스트

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+//const Schema = mongoose.Schema; Object destructuring; javascript에서 이게 아래랑 같음?
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: String,
+    age: {
+        type: Number,
+        min: 18,
+        max: 50
+    },
+    enrolled: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model('User',userSchema);
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const server = express();
+const User = require('./models/User');
 require('dotenv').config({ path: 'variables.env' });
 
+server.get('/', (req, res) => {
+    const newUser = new User();
+    newUser.email = 'danny@gmail.com';
+    newUser.name = 'danny';
+    newUser.age = 25;
+    newUser.save()
+        .then((user) => {
+            console.log(user);
+            res.json({
+                message: 'User Created Successfully'
+            });
+        })
+        .catch((err) => {
+            res.json({
+                message: 'User was not Successfully created'
+            });
+        });
+});
 
 server.listen(3000, (err) => {
     if (err) {
