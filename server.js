4af23a61bf30afaeb8cd8619884efd0465fbc851
snowynam <snowynam1@gmail.com>
2020-02-15 16:21:31 +0900

    post API호출 및 body-parser 사용

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const server = express();
+
+//express 서버에서 bodyParser.json()을 사용할 수 있도록 함.
+server.use(bodyParser.json());
 
 const users = [
     {
@@ -16,6 +20,12 @@ const users = [
 ];
 
 server.get("/api/user", (req,res) =>{
+    res.json(users);
+});
+
+server.post("/api/user", (req,res) =>{
+    console.log(req.body);
+    users.push(req.body);
     res.json(users);
 });
 
