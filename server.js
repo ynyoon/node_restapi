add4f8fb43cd9711b7f34c2d7385ceefa85dacc5
snowynam <snowynam1@gmail.com>
2020-02-15 23:11:50 +0900

    connect mongodb

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const server = express();
+require('dotenv').config({ path: 'variables.env' });
+
+
+server.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        mongoose.connect(process.env.MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("connected to database successfully");
+                console.log("The server is running");
+            }
+        });
+    }
+});
