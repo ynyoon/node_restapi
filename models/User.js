f11fa367118ca2303c3b407cbe27e8aa2e50f4f8
snowynam <snowynam1@gmail.com>
2020-07-27 17:57:58 +0900

    몽고디비 url 변경, 주석추가

--- models/User.js
+++ models/User.js
@@ -3,23 +3,20 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    name: String,
-    age: {
-        type: Number,
-        min: 18,
-        max: 50
-    },
-    enrolled: {
-        type: Date,
-        default: Date.now
+        email: {
+            type: String,
+            required: true
+        },
+        name: String,
+        age: {
+            type: Number,
+            min: 18,
+            max: 50
+        }
+    }, 
+    {
+        timestamps: true
     }
-}, {
-    timestamps: true
-}
 );
 
 module.exports = mongoose.model('User',userSchema);
